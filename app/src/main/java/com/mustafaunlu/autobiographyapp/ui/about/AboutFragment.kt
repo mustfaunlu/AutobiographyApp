19fc0a2e014c6d6763bf671ba27b24859f1fdb2c
Mustafa ÜNLÜ <mustafaunlu@mail.com>
2023-04-20 22:49:20 +0300

    about screen modified

--- app/src/main/java/com/mustafaunlu/autobiographyapp/ui/about/AboutFragment.kt
+++ app/src/main/java/com/mustafaunlu/autobiographyapp/ui/about/AboutFragment.kt
@@ -1,12 +1,11 @@
 package com.mustafaunlu.autobiographyapp.ui.about
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mustafaunlu.autobiographyapp.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.mustafaunlu.autobiographyapp.databinding.FragmentAboutBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,15 +14,18 @@ class AboutFragment : Fragment() {
 
     private lateinit var viewModel: AboutViewModel
     lateinit var binding: FragmentAboutBinding
+    private val args: AboutFragmentArgs by navArgs()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View {
         binding = FragmentAboutBinding.inflate(inflater, container, false)
+        binding.apply {
+            aboutTitleTxt.text = "about me"
+            aboutDescriptionTxt.text = args.personAbout
+        }
         return binding.root
     }
-
-
-
 }
--- app/src/main/java/com/mustafaunlu/autobiographyapp/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/mustafaunlu/autobiographyapp/ui/about/AboutViewModel.kt
@@ -3,5 +3,6 @@ package com.mustafaunlu.autobiographyapp.ui.about
 import androidx.lifecycle.ViewModel
 
 class AboutViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+
 }
