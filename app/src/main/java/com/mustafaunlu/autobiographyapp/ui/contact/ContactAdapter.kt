bc14c73c42319ceadadf2dbd4f063f156769629b
Mustafa ÜNLÜ <mustafaunlu@mail.com>
2023-04-20 22:48:56 +0300

    contact screen modified

---
+++ app/src/main/java/com/mustafaunlu/autobiographyapp/ui/contact/ContactAdapter.kt
@@ -0,0 +1,32 @@
+package com.mustafaunlu.autobiographyapp.ui.contact
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mustafaunlu.autobiographyapp.data.models.Social
+import com.mustafaunlu.autobiographyapp.databinding.ContactItemBinding
+import com.mustafaunlu.autobiographyapp.utility.loadImage
+
+class ContactAdapter(private val contactList: Array<Social>) : RecyclerView.Adapter<ContactViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = ContactItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ContactViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return contactList.size
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        holder.bind(contactList[position])
+    }
+
+}
+
+class ContactViewHolder(private val binding: ContactItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(contact: Social) {
+        binding.apply {
+            contactImg.loadImage(contact.image)
+        }
+    }
+}
--- app/src/main/java/com/mustafaunlu/autobiographyapp/ui/contact/ContactFragment.kt
+++ app/src/main/java/com/mustafaunlu/autobiographyapp/ui/contact/ContactFragment.kt
@@ -5,17 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.mustafaunlu.autobiographyapp.R
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.mustafaunlu.autobiographyapp.databinding.FragmentContactBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ContactFragment : Fragment() {
 
+    private lateinit var binding: FragmentContactBinding
+    private val args: ContactFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+    ): View {
+        binding = FragmentContactBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            contactRecyclerView.layoutManager = layoutManager
+            val adapter = ContactAdapter(args.personSocials)
+            contactRecyclerView.adapter = adapter
+        }
     }
 }
