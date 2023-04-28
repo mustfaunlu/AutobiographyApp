25a8603bf4ce5703ff7a6fec7ec20ddf79900bdf
Mustafa ÜNLÜ <mustafaunlu@mail.com>
2023-04-28 18:27:15 +0300

    webview client and progress bar added for blogs screen

--- app/src/main/java/com/mustafaunlu/autobiographyapp/ui/blogs/BlogsFragment.kt
+++ app/src/main/java/com/mustafaunlu/autobiographyapp/ui/blogs/BlogsFragment.kt
@@ -1,10 +1,13 @@
 package com.mustafaunlu.autobiographyapp.ui.blogs
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.mustafaunlu.autobiographyapp.R
 import com.mustafaunlu.autobiographyapp.databinding.FragmentBlogsBinding
@@ -13,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class BlogsFragment : Fragment() {
     private lateinit var binding: FragmentBlogsBinding
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,7 +24,18 @@ class BlogsFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentBlogsBinding.inflate(inflater, container, false)
+        val blogWebViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                binding.blogsWebview.visibility = View.GONE
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            override fun onPageFinished(view: WebView?, url: String?) {
+                binding.blogsWebview.visibility = View.VISIBLE
+                binding.progressBar.visibility = View.GONE
+            }
+        }
         binding.blogsWebview.apply {
+            webViewClient = blogWebViewClient
             settings.javaScriptEnabled = true
             loadUrl(getString(R.string.medium_blog_url))
         }
